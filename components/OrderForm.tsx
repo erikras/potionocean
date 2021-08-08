b4f91318173bb289a19465abff69dd775736bd4e
Erik Rasmussen <rasmussenerik@gmail.com>
2021-08-08 15:56:38 +0200

    Solution

--- components/OrderForm.tsx
+++ components/OrderForm.tsx
@@ -1,9 +1,12 @@
+import { useMachine } from "@xstate/react";
 import React from "react";
 import { Form } from "react-final-form";
+import { wizardMachine } from "../machine";
 import ContactInfo from "./ContactInfo";
 import Delivery, { DeliveryMethod } from "./Delivery";
 import DeliveryAddress, { House } from "./DeliveryAddress";
 import Ingredients, { Ingredient } from "./Ingredients";
+import OrderComplete from "./OrderComplete";
 
 export interface FormValues {
   firstName: string;
@@ -15,27 +18,36 @@ export interface FormValues {
 }
 
 const OrderForm = () => {
+  const [{ value }, send] = useMachine(wizardMachine);
   return (
     <div>
       <h1>Order Form</h1>
       <Form
         onSubmit={(values: FormValues) => {
-          console.log(values);
+          send({ type: "NEXT", values });
         }}
       >
-        {({ handleSubmit, submitting }) => (
+        {({ handleSubmit, submitting, values }) => (
           <form onSubmit={handleSubmit}>
-            <ContactInfo />
-            <Ingredients />
-            <Delivery />
-            <DeliveryAddress />
-            {/* <OrderComplete isDelivery /> */}
-            <div>
-              <button type="button">« Previous</button>
-              <button type="submit" disabled={submitting}>
-                Next »
-              </button>
-            </div>
+            {value === "contactInfo" && <ContactInfo />}
+            {value === "ingredients" && <Ingredients />}
+            {value === "delivery" && <Delivery />}
+            {value === "deliveryAddress" && <DeliveryAddress />}
+            {value === "orderComplete" && (
+              <OrderComplete isDelivery={values.delivery === "delivery"} />
+            )}
+            {value !== "orderComplete" && (
+              <div>
+                {value !== "contactInfo" && (
+                  <button type="button" onClick={() => send("PREVIOUS")}>
+                    « Previous
+                  </button>
+                )}
+                <button type="submit" disabled={submitting}>
+                  {value === "deliveryAddress" ? "Complete Order" : "Next »"}
+                </button>
+              </div>
+            )}
           </form>
         )}
       </Form>
